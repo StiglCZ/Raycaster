57f316927380971c3403a208022a6ab7d423c284
Stigl <stigl@ireview.games>
2025-04-23 17:50:11 +0200

    Changed to realtime

--- program.cpp
+++ program.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include <vector>
 #include "vector2.hh"
+namespace Raylib {
+    #include <raylib.h>
+};
 
 typedef unsigned char u8;
-const int W = 1440, H = 900;
+const int W = 800, H = 600;
 const float
         FOV    = 90.0f * M_PI / 180.0f,
         MaxDst = 1000;
@@ -17,35 +20,27 @@ void Export(u8* Data, std::string FileName) {
     ofs.close();
 }
 
-float LineCol(const Vector2& ray, Vector2 lineA, Vector2 lineB, float maxDst = 100) {
-    float x1 = 0, y1 = 0;
+float LineCol(Vector2 Origin, const Vector2& ray, Vector2 lineA, Vector2 lineB, float maxDst = 100) {
+    float x1 = Origin.x, y1 = Origin.y;
     float x2 = ray.x * maxDst, y2 = ray.y * maxDst;
     float x3 = lineA.x, y3 = lineA.y;
     float x4 = lineB.x, y4 = lineB.y;
 
     float uA = ((x4-x3)*(y1-y3)-(y4-y3)*(x1-x3))/((y4-y3)*(x2-x1)-(x4-x3)*(y2-y1));
     float uB = ((x2-x1)*(y1-y3)-(y2-y1)*(x1-x3))/((y4-y3)*(x2-x1)-(x4-x3)*(y2-y1));
-    if(uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
-        float mag = (Vector2){uA * (x2 - x1), uA * (y2 - y1)}.mag();
-        return mag;
-    }
+    if(uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1)
+        return (Vector2){uA * (x2 - x1), uA * (y2 - y1)}.mag();
     return maxDst + 1;
 }
 
-int main() {
-    u8* Output = new u8[W * H];
-    std::vector<Line> Scene = {
-        {{-1, 5}, {1, 3}},
-        {{-1, 5}, {-2, 3}},
-    };
-    
-    for(int i =0; i < W; i++) {
-        float Angle = FOV / W * (int)(i - W / 2);
+void RenderScreen(const std::vector<Line>& Scene, Vector2 Origin) {
+    for(int x =0; x < W; x++) {
+        float Angle = FOV / W * (int)(x - W / 2);
         Vector2 Ray = Vector2::normal({ sinf(Angle), cosf(Angle) });
 
         float MinDst = MaxDst + 1;
         for(Line line : Scene) {
-            float dst = LineCol(Ray, line.a, line.b, MaxDst);
+            float dst = LineCol(Origin, Ray, line.a, line.b, MaxDst);
             if(dst < MinDst) MinDst = dst;
         }
         
@@ -53,9 +48,28 @@ int main() {
         ushort
             ActualDistance = H / (MinDst + 1),
             Middle = (H - ActualDistance) / 2;
-        for(int y = Middle; y < ActualDistance + Middle; y++)
-            Output[W * y + i] = 255;
+        Raylib::DrawLine(x, Middle, x, Middle + ActualDistance, Raylib::WHITE);
     }
-    Export(Output, "image");
-    delete[] Output;
+}
+
+int main() {
+    std::vector<Line> Scene = {
+        {{-1, 5}, {1, 3}},
+        {{-1, 5}, {-2, 3}},
+    };
+    float SpeedMultiplier = 2.56f;
+    Vector2 Origin = {0, 0};
+    Raylib::SetTraceLogLevel(Raylib::LOG_NONE);
+    Raylib::InitWindow(W, H, "RayCaster");
+    Raylib::SetTargetFPS(60);
+    while(!Raylib::WindowShouldClose()) {
+        Raylib::BeginDrawing();
+        Raylib::ClearBackground(Raylib::BLACK);
+        RenderScreen(Scene, Origin);
+        Raylib::EndDrawing();
+        if(Raylib::IsKeyDown(Raylib::KEY_W)) Origin.y += Raylib::GetFrameTime() * SpeedMultiplier;
+        if(Raylib::IsKeyDown(Raylib::KEY_S)) Origin.y -= Raylib::GetFrameTime() * SpeedMultiplier;
+        if(Raylib::IsKeyDown(Raylib::KEY_D)) Origin.x += Raylib::GetFrameTime() * SpeedMultiplier;
+        if(Raylib::IsKeyDown(Raylib::KEY_A)) Origin.x -= Raylib::GetFrameTime() * SpeedMultiplier;
+    }
 }
